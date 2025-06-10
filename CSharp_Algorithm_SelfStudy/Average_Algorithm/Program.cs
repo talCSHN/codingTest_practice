3913baa199582dc47bc80e86219c0065ac245108
talCSHN <yujakinasakoon@gmail.com>
2025-06-11 00:14:05 +0900

    Update Program.cs

--- CSharp_Algorithm_SelfStudy/Average_Algorithm/Program.cs
+++ CSharp_Algorithm_SelfStudy/Average_Algorithm/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // string[] answer = strings.OrderBy(s => s[n]).ThenBy(s => s).ToArray();
+            int n = 1;
+            string temp = null;
+            string[] strings = new string[] { "cnbc", "anbcd" };
+            for (int i = 0; i < strings.Length - 1; i++)
+            {
+                for (int j = i + 1; j < strings.Length; j++)
+                {
+                    if (strings[i][n] > strings[j][n])
+                    {
+                        temp = strings[i];
+                        strings[i] = strings[j];
+                        strings[j] = temp;
+                    }
+                    else if (strings[i][n] == strings[j][n])
+                    {
+                        if (strings[i].CompareTo(strings[j]) == 1)
+                        {
+                            temp = strings[i];
+                            strings[i] = strings[j];
+                            strings[j] = temp;
+                        }
+                    }
+                }
+            }
+
         }
     }
 }

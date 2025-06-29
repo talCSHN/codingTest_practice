92036575741cac8cb7a3e04bc886cee1782cb40b
PARK KWANHO <yujakinasakoon@gmail.com>
2025-06-29 15:03:44 +0900

    Title: DateTime to DateTime, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250629/DateTime to DateTime/datetime-to-datetime.py
+++ 250629/DateTime to DateTime/datetime-to-datetime.py
@@ -1,24 +1,38 @@
 a, b, c = map(int, input().split())
 
 # Please write your code here.
-elapsed_min = 0
-curr_day = 11
-curr_time = 11
-curr_min = 11
+# elapsed_min = 0
+# curr_day = 11
+# curr_time = 11
+# curr_min = 11
 
-while True:
-    if (curr_day == a and curr_time == b and curr_min == c):
-        break
+dif_day = a - 11
+dif_hour = b - 11
+dif_min = c - 11
 
-    elapsed_min += 1
-    curr_min += 1
+sex = dif_day * 24 * 60
+sex1 = dif_hour * 60
+result = sex + sex1 + dif_min
+
+if (result < 0):
+    print(-1)
+else:
+    print(result)
+
+
+# while True:
+#     if (curr_day == a and curr_time == b and curr_min == c):
+#         break
+
+#     elapsed_min += 1
+#     curr_min += 1
     
-    if (curr_min > 59):
-        curr_time += 1
-        curr_min = 0
+#     if (curr_min > 59):
+#         curr_time += 1
+#         curr_min = 0
     
-    if (curr_time >= 24):
-        curr_day += 1
-        curr_time = 0
+#     if (curr_time >= 24):
+#         curr_day += 1
+#         curr_time = 0
 
-print(elapsed_min)
+# print(elapsed_min)

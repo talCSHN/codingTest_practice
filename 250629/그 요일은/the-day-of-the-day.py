7ea0793009d7926f735a5508b47213e61d3231c0
PARK KWANHO <yujakinasakoon@gmail.com>
2025-06-29 17:04:00 +0900

    Title: 그 요일은, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250629/그 요일은/the-day-of-the-day.py
+++ 250629/그 요일은/the-day-of-the-day.py
@@ -3,7 +3,8 @@ A = input()
 
 # Please write your code here.
 days_of_months = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-days_of_week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+# days_of_week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+days_of_week = ['AAA', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 elapsed_days = 1
 while True:
     if (m1 == m2 and d1 == d2):
@@ -19,7 +20,7 @@ while True:
 day_count = elapsed_days // 7
 day_count_else = (elapsed_days % 7)
 
-if (day_count_else >= days_of_week.index(A) and A != 'Sun'):
+if (day_count_else >= days_of_week.index(A)):
     day_count += 1
     print(day_count)
 else:

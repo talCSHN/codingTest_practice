e3f1cb54f2dabedd469b9ce7de1b6ac9a582c3cc
PARK KWANHO <yujakinasakoon@gmail.com>
2025-07-01 17:54:02 +0900

    Title: 블럭쌓는 명령2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250701/블럭쌓는 명령2/block-stacking-commands2.py
+++ 250701/블럭쌓는 명령2/block-stacking-commands2.py
@@ -1,16 +1,17 @@
-n, k = map(int, input().split())
-commands = [tuple(map(int, input().split())) for _ in range(k)]
+# 변수 선언 및 입력
+n, k = tuple(map(int, input().split()))
+segments = [
+    tuple(map(int, input().split()))
+    for _ in range(k)
+]
 
-# Please write your code here.
-init_arr = [0 for i in range(n+1)]
-max_arr = []
-count1 = 0
-# print(commands)
-for i in range(k):
-    start, end = commands[i]
-    for j in range(start, end + 1):
-        init_arr[j] = 1
-    max_arr.append(init_arr.count(1))
-    init_arr = [0 for i in range(n+1)]
-# print(max_arr)
-print(max(max_arr))
+blocks = [0] * (n + 1)
+
+# 블럭을 특정 구간에 쌓아줍니다.
+for a, b in segments:
+    for i in range(a, b + 1):
+        blocks[i] += 1
+
+# 최댓값을 구합니다.
+max_num = max(blocks)
+print(max_num)

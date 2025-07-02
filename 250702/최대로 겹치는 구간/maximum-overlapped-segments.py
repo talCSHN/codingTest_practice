9b1cc37c53c12801a484755d454c738267349820
PARK KWANHO <yujakinasakoon@gmail.com>
2025-07-02 19:11:32 +0900

    Title: 최대로 겹치는 구간, Time: 82ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 250702/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -0,0 +1,14 @@
+n = int(input())
+segments = [tuple(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+duplicate_segment = [0 for i in range(200)]
+for i in range(n):
+    if (segments[i][0] < 0 or segments[i][1] < 0):
+        start, end = segments[i] + (min(segments[i]) * -1)
+    else:
+        start, end = segments[i]
+    for j in range(start, end):
+        duplicate_segment[j - 1] += 1
+
+print(max(duplicate_segment))

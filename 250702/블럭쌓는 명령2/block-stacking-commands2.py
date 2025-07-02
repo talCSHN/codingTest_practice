e46f9a8e7b1f2965c53703150c6233dc494e35c1
PARK KWANHO <yujakinasakoon@gmail.com>
2025-07-02 18:38:10 +0900

    Title: 블럭쌓는 명령2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250702/블럭쌓는 명령2/block-stacking-commands2.py
+++ 250702/블럭쌓는 명령2/block-stacking-commands2.py
@@ -2,14 +2,15 @@ n, k = map(int, input().split())
 commands = [tuple(map(int, input().split())) for _ in range(k)]
 
 # Please write your code here.
-init_arr = [0 for i in range(n+1)]
+init_arr = [0 for i in range(n)]
+# print(init_arr)
 max_arr = []
 count1 = 0
 # print(commands)
 for i in range(k):
     start, end = commands[i]
     for j in range(start, end + 1):
-        init_arr[j] = 1
+        init_arr[j - 1] = 1
     # print(init_arr)
     max_arr.append(init_arr.count(1))
     init_arr = [0 for i in range(n)]

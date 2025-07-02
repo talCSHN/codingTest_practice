ce31541d0b8d021e56d671f89fd6fc5f09f38164
PARK KWANHO <yujakinasakoon@gmail.com>
2025-07-02 19:12:22 +0900

    Title: 최대로 겹치는 구간, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250702/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 250702/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -5,7 +5,8 @@ segments = [tuple(map(int, input().split())) for _ in range(n)]
 duplicate_segment = [0 for i in range(200)]
 for i in range(n):
     if (segments[i][0] < 0 or segments[i][1] < 0):
-        start, end = segments[i] + (min(segments[i]) * -1)
+        start = segments[i][0] + (min(segments[i]) * -1)
+        end = segments[i][1] + (min(segments[i]) * -1)
     else:
         start, end = segments[i]
     for j in range(start, end):

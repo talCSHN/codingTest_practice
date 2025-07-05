bf5d3545de40690be12d1b30e7e056882bbd3739
PARK KWANHO <yujakinasakoon@gmail.com>
2025-07-05 18:09:45 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 133ms, Memory: 49MB, Status: Runtime Error - Codetree

--- 250705/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 250705/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -7,17 +7,20 @@ x1[1], y1[1], x2[1], y2[1] = map(int, input().split())
 OFFSET = 1000
 rectangles = [[x1[i], y1[i], x2[i], y2[i]] for i in range(2)]
 init_width = [[0]*2001 for _ in range(2001)]
-for x11, y11, x22, y22 in rectangles:
-    x11 = x11 + OFFSET
-    y11 = y11 + OFFSET
-    x22 = x22 + OFFSET
-    y22 = y22 + OFFSET
-    for x in range(x1[0], x2[0]):
-        for y in range(y1[0], y2[0]):
-            init_width[x][y] = 1
-    for x in range(x1[1], x2[1]):
-        for y in range(y1[1], y2[1]):
-            init_width[x][y] = 0
+for a1, b1, a2, b2 in rectangles:
+    a1 = a1 + OFFSET
+    b1 = b1 + OFFSET
+    a2 = a2 + OFFSET
+    b2 = b2 + OFFSET
+    if (a1 == x1[0]+OFFSET):
+        for x in range(a1, a2):
+            for y in range(b1, b2):
+                init_width[x][y] = 1
+    else:
+        for x in range(a1, a2):
+            for y in range(b1, b2):
+                init_width[x][y] = 0
+
 xArr = []
 yArr = []
 for x in range(2001):
@@ -25,6 +28,9 @@ for x in range(2001):
         if(init_width[x][y] == 1):
             xArr.append(x)
             yArr.append(y)
-
+# xArr.sort()
+# yArr.sort()
 coverWidth = (max(xArr) - min(xArr) + 1) * (max(yArr) - min(yArr) + 1)
 print(coverWidth)
+# print(xArr)
+# print(yArr)

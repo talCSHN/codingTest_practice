d8ab260bf7798402dafac19dca74bad7cbc1c1d4
PARK KWANHO <yujakinasakoon@gmail.com>
2025-07-07 22:33:52 +0900

    Title: 벌금은 누구에게, Time: 81ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 250707/벌금은 누구에게/who-will-pay.py
+++ 250707/벌금은 누구에게/who-will-pay.py
@@ -5,10 +5,14 @@ student = [int(input()) for _ in range(M)]
 # Please write your code here.
 life_counts = [K for _ in range(N)]
 # [5 5 5 5 5]
-
+iterator = 0
 for i in student:   # 2 5 2 3 5 2 4
-    while(life_counts.count(0) == 0):
-        life_counts[i - 1] -= 1
+    while(iterator <= 6): # 0 1 2 3 4 5
+        if(life_counts.count(0) == 0):
+            life_counts[i - 1] -= 1
+        if(iterator == 6 and life_counts.count(0) == 0):
+            print(-1)
+        iterator += 1
     print(i)
     break
 
